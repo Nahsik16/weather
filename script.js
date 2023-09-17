1e7a71991a29e251b27c46a2de8083d380706ffc
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2023-09-17 15:37:24 +0530

    edited

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey="43fed0b90e39102924226253d967ee22";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+  const response =await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  document.querySelector(".city").innerHTML= data.name;
+  document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + " °C";
+  document.querySelector(".humidity").innerHTML= data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML= data.wind.speed + " km/h";
+
+  document.querySelector(".weather").style.display = "block";
+}
+
+searchBtn.addEventListener('click', 
+()=>{
+  checkWeather(searchBox.value);
+})
