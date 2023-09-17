9de0f00f03c87887a205d77f34bf5720c292787e
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2023-09-17 17:57:06 +0530

    completed

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city){
   const response =await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
-  console.log(data);
+ 
 
   document.querySelector(".city").innerHTML= data.name;
   document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + " °C";
